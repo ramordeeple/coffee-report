a0fda7e71a90b4fe904da58869f583f14d540f79
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-22 21:23:27 +0300

    Add runner

---
+++ src/app/runner.py
@@ -0,0 +1,22 @@
+"""
+Связывает загрузку данных, выбор стратегии, получение результата
+"""
+
+from typing import List
+
+from src.app.factory import ReportFactory
+from src.app.processor import DataProcessor
+from src.domain.constants import NOT_FOUND_DATA
+from src.domain.models import ReportResult
+
+
+class ReportRunner:
+    @staticmethod
+    def run(file_paths: List[str], report_type: str) -> ReportResult:
+        raw_data = DataProcessor.collect_data(file_paths)
+        if not raw_data:
+            raise ValueError(NOT_FOUND_DATA)
+
+        strategy = ReportFactory.get_report(report_type)
+
+        return strategy.generate(raw_data)
