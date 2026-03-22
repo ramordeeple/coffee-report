beb6bd4d4670fb4dd5d9b40588104ff97f0afbbc
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-22 19:46:38 +0300

    Update constants.py by adding precision for decimal

--- src/domain/constants.py
+++ src/domain/constants.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from typing import Final
 
 ENCODING: Final[str] = "utf-8"
@@ -7,3 +8,5 @@ COLUMN_STUDENT: Final[str] = "student"
 COLUMN_COFFEE_SPENT: Final[str] = "coffee_spent"
 
 REPORT_MEDIAN_COFFEE: Final[str] = "median_coffee"
+
+DECIMAL_PRECISION: Final[Decimal] = Decimal("0.01")
