41e983a9aeaf512af7a7ceea36da0f403738308f
scarabush_scarabush <55894795+ramordeeple@users.noreply.github.com>
2026-03-22 18:49:49 +0300

    Add constants.py

---
+++ src/domain/constants.py
@@ -0,0 +1,9 @@
+from typing import Final
+
+ENCODING: Final[str] = "utf-8"
+CSV_EXTENSION: Final[str] = ".csv"
+
+COLUMN_STUDENT: Final[str] = "student"
+COLUMN_COFFEE_SPENT: Final[str] = "coffee_spent"
+
+REPORT_MEDIAN_COFFEE: Final[str] = "median_coffee"
